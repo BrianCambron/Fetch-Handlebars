f43486f6aeb569c3c8b288e31a67ef01317b2a71
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-23 19:13:46 -0400

    have the aside picture name and login name done

--- main.js
+++ main.js
@@ -3,15 +3,19 @@ fetch('https://api.github.com/users/BrianCambron')
   .then(data => {
     console.log(data);
     let avatar_url = data.avatar_url;
+    let name = data.names;
+    let login = data.login;
 
     // .catch(error => console.log(error));
     const source = document.getElementById("entry-template").innerHTML;
     // console.log('source', source);
     const template = Handlebars.compile(source);
     const context = {
-      avatar_url: data.avatar_url
+      avatar_url: data.avatar_url,
+      name: data.name,
+      login: data.login,
     };
     const html = template(context);
-    console.log(html);
+    // console.log(html);
     document.querySelector('.container').innerHTML = html;
-  })
+  });
