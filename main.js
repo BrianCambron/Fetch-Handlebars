4bed07974171ea0b3c1e9989d0566d7e2835dc3d
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-23 20:17:20 -0400

    started on the organizations but having asyn issues

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ fetch('https://api.github.com/users/BrianCambron')
     let avatar_url = data.avatar_url;
     let name = data.names;
     let login = data.login;
+    let followers_url = data.followers_url;
+    let following_url = data.following_url;
+    let starred_url = data.starred_url;
+    // console.log(following_url);
+    // console.log(starred_url);
+    // console.log(starred_url);
 
     // .catch(error => console.log(error));
     const source = document.getElementById("entry-template").innerHTML;
@@ -14,8 +20,25 @@ fetch('https://api.github.com/users/BrianCambron')
       avatar_url: data.avatar_url,
       name: data.name,
       login: data.login,
+      followers_url: data.followers_url,
+      following_url: data.following_url,
+      starred_url: data.starred_url,
     };
     const html = template(context);
     // console.log(html);
     document.querySelector('.container').innerHTML = html;
+  });
+  fetch('https://api.github.com/users/BrianCambron/orgs')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+      let avatar_url2 = data[0].avatar_url;
+      console.log(avatar_url2);
+      const source = document.getElementById("entry-template").innerHTML;
+      const template = Handlebars.compile(source);
+      const context = {
+        avatar_url2: data[0].avatar_url,
+      };
+      const html = template(context);
+      // document.querySelector('.container').innerHTML = html;
   });
